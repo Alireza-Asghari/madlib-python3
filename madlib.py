b074e5906572c7a18753ff9f623056c79add3985
Alireza <asgharialireza1995@gmail.com>
2021-10-02 13:56:16 +0330

    madlib

--- madlib.py
+++ madlib.py
@@ -1,6 +1,12 @@
-'''
-youtube = 'ALireza'
-print("youtuber" + " " + youtube)
-print('youtuber is {}'.format(youtube))
-print (f'youtuber is {youtube}')
-'''
+
+
+adj = input("Adjective: ")
+verb1 = input('verb1: ')
+verb2 = input('verb2: ')
+famous_person = input('famous person: ')
+
+madlib = f"""Computer programming is so {adj}! it makes me so excited because
+ \
+ I love to {verb1}. Stay hyrated and {verb2} like you are {famous_person} """
+
+print(madlib) 
